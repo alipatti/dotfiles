8a4472fa28521eb1be467cbcdede80f53513095d
Alistair Pattison <alistairpattison@gmail.com>
2023-09-03 18:04:59 -0500

    update path, clean up

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,10 @@
+# set editor
+set -gx EDITOR nvim
+
+# update path
+fish_add_path ~/.cargo/bin
+fish_add_path ~/.local/bin
+
 zoxide init fish | source
-thefuck --alias | source
 starship init fish | source
 rtx activate fish | source
