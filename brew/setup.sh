8befa782b398b43e876c0b68323aea7e34872c6b
Alistair Pattison <alistairpattison@gmail.com>
2024-08-12 16:44:53 -0400

    cleanup

--- brew/setup.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-if ! command -v brew &>/dev/null; then
-    echo "Brew not found. Installing it..."
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-else
-    echo "Brew installation found. Continuing..."
-fi
