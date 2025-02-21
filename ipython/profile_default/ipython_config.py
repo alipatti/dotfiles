89e436ac9a3f5558ff3032cfccfdee448cd669f8
Alistair Pattison <alistairpattison@gmail.com>
2025-02-21 14:08:18 -0500

    add ipython config

---
+++ ipython/profile_default/ipython_config.py
@@ -0,0 +1,8 @@
+# type: ignore
+
+c.InteractiveShellApp.exec_lines = [
+    "%load_ext autoreload",
+    "%autoreload complete",
+]
+c.TerminalInteractiveShell.confirm_exit = False
+c.TerminalIPythonApp.display_banner = False
