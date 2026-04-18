595565d0b837553259d9b6f750d00d7620bd2bec
Alistair Pattison <alistairpattison@gmail.com>
2026-04-18 18:38:36 -0400

    fix bug with env loading

--- fish/functions/load_dotenv.fish
+++ fish/functions/load_dotenv.fish
@@ -1,7 +1,7 @@
 function load_dotenv
     for file in $argv
         # Skip files that aren't plaintext (e.g. git-crypt encrypted files)
-        if not string match -q 'text' "$(file -b --mime-type $file)"
+        if not string match -q 'text*' "$(file -b --mime-type $file)"
             continue
         end
         set vars "$(cat $file | grep '^[^#]' 2>/dev/null | string join ' ')"
