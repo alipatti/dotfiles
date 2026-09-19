ded92102e3959a16362ae12c0f043d8c389bc3ca
Alistair Pattison <alistairpattison@gmail.com>
2026-09-19 18:17:59 -0400

    custom web preview browser

--- nvim/plugin/typst.lua
+++ nvim/plugin/typst.lua
@@ -2,8 +2,15 @@ vim.pack.add({
 	{ src = "https://github.com/chomosuke/typst-preview.nvim", version = vim.version.range("1") },
 })
 
+-- see macos/webview.swift
+local webview = vim.fn.executable("webview") == 1
+
 require("typst-preview").setup({
 	dependencies_bin = { tinymist = "tinymist" },
+	-- anything on stderr is reported as a failure
+	open_cmd = webview and "webview %s 2>/dev/null" or nil,
+	-- offscreen pages are canvases in a foreignObject, which webkit misplaces
+	partial_rendering = not webview,
 	get_root = function()
 		return vim.fn.getcwd()
 	end,
